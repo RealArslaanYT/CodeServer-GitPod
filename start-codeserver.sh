25306566e5c8f6750301bd0912b89bcbd16ed605
Arslaan Pathan <xminecrafterfun@gmail.com>
2025-02-18 07:59:23 +0000

    Initial commit

---
+++ start-codeserver.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "This script only needs to be run once, and sets up code-server to autostart."
+
+docker run -d \
+  --name=code-server \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Etc/UTC \
+  -e PASSWORD=codingclub2025 \
+  -e SUDO_PASSWORD=c0dingclub-2025-root \
+  -e DEFAULT_WORKSPACE=/config/workspace \
+  -p 8443:8443 \
+  -v /path/to/code-server/config:/config \
+  --restart unless-stopped \
+  lscr.io/linuxserver/code-server:latest
